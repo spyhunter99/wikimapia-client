006a457f7e1a4de8af97d00cb0c3c1281e653af4
Thomas Gallagher <gallagth@gmail.com>
2017-09-17 18:38:48 +0200

    Add a search bar to customize the query

--- app/src/main/java/com/github/spyhunter99/wikimapia_client/MainActivity.java
+++ app/src/main/java/com/github/spyhunter99/wikimapia_client/MainActivity.java
@@ -2,12 +2,17 @@ package com.github.spyhunter99.wikimapia_client;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,14 +35,32 @@ public class MainActivity extends Activity implements WikimapiaAPI.OnCategoryRes
     private static final String API_KEY_PATH = Environment.getExternalStorageDirectory().getPath() + "/wikimapia_api_key";
 
     private WikimapiaAPI mApi;
+    private TextView mResultsView;
+    private EditText mSearchQueryEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (requestSdCardPermssion()) {
-            doRequest();
-        }
+        mResultsView = findViewById(R.id.results);
+        mSearchQueryEditText = findViewById(R.id.search_query);
+        mSearchQueryEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    if (!requestSdCardPermssion()) {
+                        Toast.makeText(MainActivity.this,
+                                "Please give permission to read api key from sdcard", Toast.LENGTH_LONG).show();
+                    } else {
+                        String text = mSearchQueryEditText.getText().toString();
+                        doRequest(text);
+                    }
+                    hideSoftKeyboard();
+                    return true;
+                }
+                return false;            }
+        });
+        requestSdCardPermssion();
     }
 
     @Override
@@ -50,23 +73,21 @@ public class MainActivity extends Activity implements WikimapiaAPI.OnCategoryRes
         switch (requestCode) {
             case PERMISSION_REQUEST_READ_EXTERNAL_STORAGE:
                 if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    doRequest();
-                } else {
+                        && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(this, "Please give permission to read api key from sdcard", Toast.LENGTH_LONG).show();
                 }
                 break;
         }
     }
 
-    private void doRequest() {
+    private void doRequest(final String query) {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     String apiKey = loadApiKeyFromSdCard();
                     mApi = new WikimapiaAPI(apiKey, WikimapiaAPI.Format.JSON);
-                    SearchResults results = mApi.getObjectsBySearchQuery("restaurant", 48.864716, 2.349014, 1, 10);
+                    SearchResults results = mApi.getObjectsBySearchQuery(query, 48.864716, 2.349014, 1, 10);
                     Timber.d(results.toString());
                     final StringBuilder builder = new StringBuilder();
                     for (Place p : results.getPlaces()) {
@@ -75,7 +96,7 @@ public class MainActivity extends Activity implements WikimapiaAPI.OnCategoryRes
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            ((TextView) findViewById(R.id.restaurant_results)).setText(builder.toString());
+                            mResultsView.setText(builder.toString());
                         }
                     });
                 } catch (IOException e) {
@@ -99,5 +120,10 @@ public class MainActivity extends Activity implements WikimapiaAPI.OnCategoryRes
     private String loadApiKeyFromSdCard() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(API_KEY_PATH));
         return reader.readLine();
+    }
+
+    private void hideSoftKeyboard() {
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.toggleSoftInput(0, 0);
     }
 }
